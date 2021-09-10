6c66592adaf8a406138438f9832ed50423ef9d8d
yunx3arch <64882509+yunx3arch@users.noreply.github.com>
2021-09-10 12:31:55 -0700

    Initial Commit

---
+++ rollADice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rollADice
+//
+//  Created by Yun Xu on 2021/9/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
